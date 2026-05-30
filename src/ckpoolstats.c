b75dca779d0fe10aa8c04374f59cb0d85fd78283
ckolivas <kernel@kolivas.org>
2026-05-30 17:13:20 +1000

    Initial stubs, build.

---
+++ src/ckpoolstats.c
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2026 Con Kolivas
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.  See COPYING for more details.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "libckpool.h"
+
+int main(int __maybe_unused argc, char __maybe_unused **argv)
+{
+	return 0;
+}
