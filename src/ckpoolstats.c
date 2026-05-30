f9d8d94332775427a9aab9c2b6988bf6a21534dd
ckolivas <kernel@kolivas.org>
2026-05-30 17:41:21 +1000

    Parse config file for an array of dirs.

--- src/ckpoolstats.c
+++ src/ckpoolstats.c
@@ -12,7 +12,43 @@
 
 #include "libckpool.h"
 
+#define log(fmt, ...) do { \
+	printf(fmt, ##__VA_ARGS__); \
+	printf("\n"); \
+	fflush(stdout); \
+} while(0)
+
+#define logerr(fmt, ...) do { \
+	fprintf(stderr, fmt, ##__VA_ARGS__); \
+	fprintf(stderr, "\n"); \
+	fflush(stderr); \
+} while(0)
+
+#define fail(fmt, ...) do { \
+	fprintf(stderr, fmt, ##__VA_ARGS__); \
+	fprintf(stderr, "\n"); \
+	fflush(stderr); \
+	exit(1); \
+} while(0)
+
 int main(int __maybe_unused argc, char __maybe_unused **argv)
 {
+	json_t *conf, *dirs, *val;
+	size_t index;
+
+	conf = json_load_file("ckpoolstats.conf", 0, NULL);
+	if (!conf)
+		fail("Failed to load ckpoolstats.conf");
+	dirs = json_object_get(conf, "dirs");
+	if (!dirs || !json_is_array(dirs))
+		fail("dirs array not found");
+
+	json_array_foreach(dirs, index, val) {
+		const char *dir = json_string_value(val);
+		log("Found dir %s", dir);
+	}
+
+	json_decref(conf);
+
 	return 0;
 }
